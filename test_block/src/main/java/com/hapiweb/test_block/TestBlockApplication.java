af7a001417aaedfcd472e084b751e9bb7fc5a849
Yang Yang Zhang <1334054279@qq.com>
2021-05-17 17:55:25 +0800

    test1

--- test_block/src/main/java/com/hapiweb/test_block/TestBlockApplication.java
+++ test_block/src/main/java/com/hapiweb/test_block/TestBlockApplication.java
@@ -1,9 +1,11 @@
 package com.hapiweb.test_block;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.hapiweb.test_block.resource.mapper")
 public class TestBlockApplication {
 
     public static void main(String[] args) {
