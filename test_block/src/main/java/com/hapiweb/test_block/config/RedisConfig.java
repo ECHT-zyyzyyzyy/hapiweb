ddfe7b7d2e7d99172c14952a18f88a99b1887284
ECHT-zyyzyyzyy <1334054279@qq.com>
2021-05-20 00:33:01 +0800

    修复 mybatis gennerator bug

---
+++ test_block/src/main/java/com/hapiweb/test_block/config/RedisConfig.java
@@ -0,0 +1,14 @@
+package com.hapiweb.test_block.config;
+
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class RedisConfig extends CachingConfigurerSupport {
+
+    //todo
+
+}
